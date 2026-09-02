311d4785dda55d3cd232a18c2b869eabb0022606
RamosDev <rangerskiper@gmail.com>
2026-09-01 20:01:35 -0400

    added fontawesome icons

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -28,21 +28,7 @@
       <li>
         <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
           <!-- Settings icon -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            stroke-width="2"
-            fill="none"
-            stroke="currentColor"
-            class="my-1.5 inline-block size-4"
-          >
-            <path d="M20 7h-9"></path>
-            <path d="M14 17H5"></path>
-            <circle cx="17" cy="17" r="3"></circle>
-            <circle cx="7" cy="7" r="3"></circle>
-          </svg>
+          <i class="fa-solid fa-book"></i>
           <span class="is-drawer-close:hidden">{{ categoria.name }}</span>
         </button>
       </li>
